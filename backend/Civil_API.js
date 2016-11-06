4943ca34d326cb65f6d943a521c7b1b0f6c96024
alex4864 <shadleyalex@gmail.com>
2016-11-06 15:45:31 -0600

    changes read format of VoterData from XML to JSON, adds demo html file

--- backend/Civil_API.js
+++ backend/Civil_API.js
@@ -2,6 +2,7 @@ function VoterData(address, key) { //voter data class
     this.address = address.replace(" ", "+");
     this.key = key;
     this.xmlData = new XMLHttpRequest();
+    this.parser = new DOMParser();
 }
 
 VoterData.prototype.getData = function() {
@@ -13,10 +14,14 @@ VoterData.prototype.print = function() {
     console.log(this.xmlData.responseText);
 };
 
-VoterData.prototype.getXmlDoc = function() {
-    return this.xmlData;
+VoterData.prototype.getJSON = function() {
+    return JSON.parse(this.xmlData.responseText);
 };
 
-var dat = new VoterData("6807 E 82nd St Tulsa OK", "AIzaSyAHNL-KlQfG8iva2YZITyxCQT4oEQbGr4Q");//replace spaces with '+' for address
+var dat = new VoterData("1734 Engel Rd Lawrence KS", "AIzaSyAHNL-KlQfG8iva2YZITyxCQT4oEQbGr4Q");//replace spaces with '+' for address
 dat.getData();
 dat.print();
+var j = dat.getJSON()
+console.log(j);
+document.getElementById("Poll_Name").innerHTML = j['pollingLocations'][0]['address']['locationName'];
+document.getElementById("Poll_Location").innerHTML = j['pollingLocations'][0]['address']['line1'] + " " + j['pollingLocations'][0]['address']['city'] + ", " + j['pollingLocations'][0]['address']['state'];
