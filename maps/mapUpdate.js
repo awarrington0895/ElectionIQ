6d8dcabd2b42a998d8be102d71b0da03dbe57dc5
zwelk <welk.zachary@gmail.com>
2016-11-06 19:56:08 -0600

    Made it a little more extendable
    Modified maps.html and addedd mapUpdate.js

---
+++ maps/mapUpdate.js
@@ -0,0 +1,5 @@
+function mapPrep(location, id, query)
+{
+    var root = 'https://www.google.com/maps/embed/v1/search?key=AIzaSyDko3US7QPjYurzRs1V7rtB344Fc9G25Ck&q=';
+    document.getElementById(id).src = root.concat(query, "+near+", location);
+}
